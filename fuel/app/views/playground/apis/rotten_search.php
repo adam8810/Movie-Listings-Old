ee864258c7e6f3e9f71fa35115628e8f9a908d46
Adam Booth <adam@ambooth.com>
2012-03-14 15:28:42 -0500

    Switching from tables to divs, added js to handle adding movies to db
    Divs will eventually be used to make things a little more visually appealing, floating and such. I'll have different views the user can toggle between.
    
    Js is used to communicate with the database model to add movies currently.

--- fuel/app/views/playground/apis/rotten_search.php
+++ fuel/app/views/playground/apis/rotten_search.php
@@ -2,12 +2,27 @@
     <head>
         <?= Asset::CSS('rotten.css'); ?>
 
-        <?= Asset::js('jquery.js'); ?>
-        <script>
-            $(document).ready(function() {
+        <?= Asset::js('jquery.js'); ?>       
+        <script type="text/javascript">
+            $('document').ready(function()
+            {
+                
+                $('a').click(function(e)
                 {
-                    alert('hi');
-                }
+                    // Prevent from navigating away from page
+                    e.preventDefault();
+                                    
+                    // Use Ajax call to interact with database
+                    $.ajax({
+                        url: this,
+                        success: function(){
+                            
+                        }                        
+                    })
+                                    
+                });
+            });
+            
     
         </script>
 
@@ -24,37 +39,42 @@
                     <?= Form::submit(); ?>
                     <?= Form::close(); ?>
                 </div>
-                <table>
-                    <?php
-                    if ($movie != null)
-                    {
-                        foreach ($movie as $m)
-                        {
-                            ?>
-                            <tr>
-                                <td>
-                                    <div class="img_wrapper">
-                                        <img height="200" width="145" src="<?= $m->img; ?>"/>
-                                        <div class="movie_add_btn">
-                                            <a class="add_link" href="#">Add</a>
-                                        </div>
 
-                                    </div>
-                                </td>
-                                <td>
-                                    <ul>
-                                        <li>Title: <?= $m->title; ?></li>
-                                        <li>Year: <?= $m->year; ?></li>
-                                        <li>MPAA Rating: <?= $m->m_rating; ?></li>
-                                        <li><a href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=1">Add To Viewed List</a></li>
-                                        <li><a href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=0">Add to Wish List!</a></li>
-                                    </ul>
-                                </td></tr>
-                            <?php
-                        }
+                <?php
+                if ($movie != null)
+                {
+                    foreach ($movie as $m)
+                    {
+                        ?>
+                        <div>
+                            <table>
+                                <tr>
+                                    <td>
+                                        <div class="img_wrapper">
+                                            <img height="200" width="145" src="<?= $m->img; ?>"/>
+                                            <div class="movie_add_btn">
+                                                <a class="add_link" id="<?= $m->id; ?>" href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=1">Add</a>
+                                            </div>
+
+                                        </div>
+                                    </td>
+                                    <td class="movie_info">
+                                        <ul>
+                                            <li>Title: <?= $m->title; ?></li>
+                                            <li>Year: <?= $m->year; ?></li>
+                                            <li>MPAA Rating: <?= $m->m_rating; ?></li>
+                                            <li><a id="<?= $m->id; ?>" href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=1">Add To Viewed List</a></li>
+                                            <li><a href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=0">Add to Wish List!</a></li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <?php
                     }
-                    ?>
-                </table>
+                }
+                ?>
+
             </div>
 
 
