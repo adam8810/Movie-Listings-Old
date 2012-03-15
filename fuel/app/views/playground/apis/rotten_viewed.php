14646558cbe2e737523e988e61e83daa1d65ed7d
Adam Booth <adam@ambooth.com>
2012-03-15 03:16:35 -0500

    Finished transitioning everything from tables to divs
    Working just like I expected it to. Yay!

--- fuel/app/views/playground/apis/rotten_viewed.php
+++ fuel/app/views/playground/apis/rotten_viewed.php
@@ -15,7 +15,7 @@
                     $('.movie_btn').css('display','none')
                 })
                 
-                $('.button_link').click(function(e)
+                $('.button_link.remove').click(function(e)
                 {
                     // Prevent from navigating away from page
                     e.preventDefault();
@@ -27,26 +27,56 @@
                             
                         }
                     })
+                    
+                    $('.movie_item#' + this.id).animate({
+                        height:0, width:0, opacity: .5
+                    }, 100, "linear", function(){
+                        $('.movie_item#' + this.id).css('display','none')
+                    } );
                                     
                 });
                 
-                $('.view_btn').click(function(e){
+                
+                $('a.view_btn').click(function(e){
                     e.preventDefault();
+                    
                     
                     switch(this.id)
                     {
                         case 'list':
-                            $('tr.tr_image').css('display','none');
-                            $('td.title').css('display','none');
+                            $('.img_wrapper').css('display','none');
+                            $('span.header').css('display','none');
+                            $('div.movie_item').css('padding','5px');
+                            $('.content.title').css('width', 400);
+                            $('.movie_item').css('height','50px');
+                            $('.movie_item').css('margin-bottom','0px');
+                            $('.content.runtime').css('width', 100);
+                            $('.content.year').css('width', 50);
+                            $('.content.m_rating').css('width', 70);
+                            $('div.content').css('float', 'left');
+                            $('div#movie_head').css('display', 'block');
+                            break;
+                            
+                        case 'albumlist':
+                            $('.img_wrapper').css('display','block');
+                            $('span.header').css('display','inline');
+                            $('div.movie_item').css('padding','5px 10px 10px 150px');
+                            
+                            $('.content').css('width', '100%');
+                            $('.movie_item').css('height','180px');
+                            $('.movie_item').css('margin-bottom','10px');
+                            $('div.content').css('float', 'inline');
+                            $('div#movie_head').css('display', 'none');
                             
                             break;
-                        case 'albumlist':
-                            $('tr.tr_image').css('display','table-row');
-                            $('td.title').css('display','table-row');
-                            break;
                         case 'grid':
-                            $('tr.tr_image').css('display','table-row');
-                            $('td.title').css('display','table-row');
+                            $('.img_wrapper').css('display','block');
+                            $('span.header').css('display','inline');
+                            $('div.content').css('display','none');
+                            $('div.movie_item').css('width','145px');
+                            $('div.movie_item').css('padding','0px');
+                            $('div.movie_item').css('float','left');
+                            $('div#movie_info_box').css('width','1015px');
                             break;
                     }
                 })
@@ -75,7 +105,12 @@
                 </div>
             </div>
             <div class="clearer"></div>
-
+            <div id="movie_head">
+                <div class="title movie_head_item content">Title</div>
+                <div class="runtime movie_head_item content">Runtime</div>
+                <div class="year movie_head_item content">Year</div>
+                <div class="m_rating movie_head_item content">MPAA Rating</div>
+            </div>
             <div id="movie_info_box">
                 <?php
                 if ($movie != null && 1)
@@ -83,23 +118,20 @@
                     foreach ($movie as $m)
                     {
                         ?>
-                        <div id="<?= $m['ID']; ?>">
-                            <table class="movie_table">
-                                <tr class="tr_image">
-                                    <td rowspan="7">
-                                        <div class="img_wrapper">
-                                            <img height="200" width="145" src="<?= $m['image']; ?>"/>
-                                            <div class="movie_btn">
-                                                <a class="button_link" id="<?= $m['ID']; ?>" href="remove_movie/<?= $m['ID']; ?>">Remove</a>
-                                            </div>
-                                        </div>
-                                    </td>
-                                <tr class="tr_item"><td class="header title">Title</td><td class="content"><?= $m['title']; ?></td></tr>
-                                <tr class="tr_item"><td class="header runtime">Runtime</td><td class="content"><?= (floor($m['runtime'] / 60) > 1 ? floor($m['runtime'] / 60) . ' hours' : floor($m['runtime'] / 60) . ' hour'); ?> and <?= $m['runtime'] % 60; ?> minutes</td></tr>
-                                <tr class="tr_item"><td class="header year">Year</td><td class="content"><?= $m['year']; ?></td></tr>
-                                <tr class="tr_item"><td class="header m_rating">MPAA Rating</td><td class="content"><?= $m['m_rating']; ?></td></tr>
-                                </tr>
-                            </table>
+                        <div class="movie_item" id="<?= $m['ID']; ?>">
+
+                            <div class="img_wrapper">
+                                <img height="200" width="145" src="<?= $m['image']; ?>"/>
+                                <div class="movie_btn">
+                                    <a class="button_link remove" id="<?= $m['ID']; ?>" href="remove_movie/<?= $m['ID']; ?>">Remove</a>
+                                </div>
+                            </div>
+                            <div class="content title"><span class="header title">Title: </span><?= $m['title']; ?></div>
+                            <div class="content runtime"><span class="header runtime">Runtime: </span><?= (floor($m['runtime'] / 60) > 1 ? floor($m['runtime'] / 60) . ' hours' : floor($m['runtime'] / 60) . ' hour'); ?> and <?= $m['runtime'] % 60; ?> minutes</div>
+                            <div class="content year"><span class="header year">Year: </span><?= $m['year']; ?></div>
+                            <div class="content m_rating"><span class="header m_rating">MPAA Rating: </span><?= $m['m_rating']; ?></div>
+                            <div class="clearer"></div>
+
                         </div>
                         <?php
                     }
