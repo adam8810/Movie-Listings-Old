c98dcb6cfe912820c561b9d13bf32a3d987a05cb
Adam Booth <adam@ambooth.com>
2012-03-15 01:45:28 -0500

    Major Changes: img_wrapper remove effect, views toggle js, HTML restructure
    Moving html elements around. Trying to get different views. Currently using tables, but I'm going to branch out and try it with divs now. Seems easier to modify how the different elements float around instead of messing with tables.

--- fuel/app/views/playground/apis/rotten_viewed.php
+++ fuel/app/views/playground/apis/rotten_viewed.php
@@ -8,8 +8,14 @@
         <script type="text/javascript">
             $('document').ready(function()
             {
+                // Hover effect of Add/Remove Button
+                $('.img_wrapper').hover(function(){
+                    $('.movie_btn').css('display','block')
+                }, function(){
+                    $('.movie_btn').css('display','none')
+                })
                 
-                $('a').click(function(e)
+                $('.button_link').click(function(e)
                 {
                     // Prevent from navigating away from page
                     e.preventDefault();
@@ -23,6 +29,27 @@
                     })
                                     
                 });
+                
+                $('.view_btn').click(function(e){
+                    e.preventDefault();
+                    
+                    switch(this.id)
+                    {
+                        case 'list':
+                            $('tr.tr_image').css('display','none');
+                            $('td.title').css('display','none');
+                            
+                            break;
+                        case 'albumlist':
+                            $('tr.tr_image').css('display','table-row');
+                            $('td.title').css('display','table-row');
+                            break;
+                        case 'grid':
+                            $('tr.tr_image').css('display','table-row');
+                            $('td.title').css('display','table-row');
+                            break;
+                    }
+                })
             });
             
     
@@ -31,41 +58,54 @@
     </head>
     <body>
         <div id="wrapper">
-            <div id="movie_info_box">
-                <div id="movie_info_box">
 
-                    <?= Form::open(array('action' => 'playground/apis/rotten/search/', 'method' => 'get')); ?>
-                    <?= Form::label('Movie Title'); ?>
-                    <?= Form::input('title'); ?>
-                    <?= Form::submit(); ?>
-                    <?= Form::close(); ?>
+            <div id="movie_form_box">
+
+                <?= Form::open(array('action' => 'playground/apis/rotten/search/', 'method' => 'get')); ?>
+                <?= Form::label('Movie Title'); ?>
+                <?= Form::input('title'); ?>
+                <?= Form::submit(); ?>
+                <?= Form::close(); ?>
+                <div id="view_controls">
+                    <ul>
+                        <li><a class="view_btn" id="list" href="#List">List</a></li>
+                        <li><a class="view_btn" id="albumlist" href="#AlbumList">Album List</a></li>
+                        <li><a class="view_btn" id="grid" href="#Grid">Grid</a></li>
+                    </ul>
                 </div>
-                <table>
-                    <?php
-                    if ($movie != null && 1)
+            </div>
+            <div class="clearer"></div>
+
+            <div id="movie_info_box">
+                <?php
+                if ($movie != null && 1)
+                {
+                    foreach ($movie as $m)
                     {
-                        foreach ($movie as $m)
-                        {
-                            ?>
-                            <tr>
-                                <td>
-                                    <div class="img_wrapper">
-                                        <img height="200" width="145" src="<?= $m['image']; ?>"/>
-                                    </div>
-                                </td>
-                                <td>
-                                    <ul>
-                                        <li>Title: <?= $m['title']; ?></li>
-                                        <li>Year: <?= $m['year']; ?></li>
-                                        <li>MPAA Rating: <?= $m['m_rating']; ?></li>
-                                        <li><a href="remove_movie/<?= $m['ID']; ?>">Remove from List</a></li>
-                                    </ul>
-                                </td></tr>
-                            <?php
-                        }
+                        ?>
+                        <div id="<?= $m['ID']; ?>">
+                            <table class="movie_table">
+                                <tr class="tr_image">
+                                    <td rowspan="7">
+                                        <div class="img_wrapper">
+                                            <img height="200" width="145" src="<?= $m['image']; ?>"/>
+                                            <div class="movie_btn">
+                                                <a class="button_link" id="<?= $m['ID']; ?>" href="remove_movie/<?= $m['ID']; ?>">Remove</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                <tr class="tr_item"><td class="header title">Title</td><td class="content"><?= $m['title']; ?></td></tr>
+                                <tr class="tr_item"><td class="header runtime">Runtime</td><td class="content"><?= (floor($m['runtime'] / 60) > 1 ? floor($m['runtime'] / 60) . ' hours' : floor($m['runtime'] / 60) . ' hour'); ?> and <?= $m['runtime'] % 60; ?> minutes</td></tr>
+                                <tr class="tr_item"><td class="header year">Year</td><td class="content"><?= $m['year']; ?></td></tr>
+                                <tr class="tr_item"><td class="header m_rating">MPAA Rating</td><td class="content"><?= $m['m_rating']; ?></td></tr>
+                                </tr>
+                            </table>
+                        </div>
+                        <?php
                     }
-                    ?>
-                </table>
+                }
+                ?>
+
             </div>
 
 
