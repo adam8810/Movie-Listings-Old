1dd2c45f24a8955b38cd327f8986f6b249294a3a
Adam Booth <adam@ambooth.com>
2012-03-14 22:49:12 -0500

    Added logic to prevent problems if movies don't have certain ratings/scores
    This is usually due to age of the movie, older movies weren't rated a lot of the time. Also created $add_link because there are at least two different add buttons in the current design.

--- fuel/app/views/playground/apis/rotten_search.php
+++ fuel/app/views/playground/apis/rotten_search.php
@@ -6,6 +6,13 @@
         <script type="text/javascript">
             $('document').ready(function()
             {
+                // Hover effect of Add/Remove Button
+                $('.img_wrapper').hover(function(){
+                    
+                    $('.movie_btn').css('display','block')
+                }, function(){
+                    $('.movie_btn').css('display','none')
+                })
                 
                 $('a').click(function(e)
                 {
@@ -45,6 +52,17 @@
                 {
                     foreach ($movie as $m)
                     {
+                        if(isset($m->r_c_rating))
+                        $temp_r_c_rating = $m->r_c_rating;
+                        else
+                            $temp_r_c_rating = 'None';
+                        
+                        if(isset($m->r_a_rating))
+                            $temp_r_a_rating = $m->r_a_rating;
+                        else
+                            $temp_r_a_rating = 'None';
+                        
+                        $add_link = '../rotten/add_movie/' . $m->id . '/?title=' . urlencode($m->title) . '&year=' . $m->year . '&img=' . urlencode($m->img) . '&m_rating=' . $m->m_rating . '&viewed=1&r_a_rating=' . $temp_r_a_rating . '&r_c_rating='.$temp_r_c_rating. '&r_a_score=' . $m->r_a_score . '&r_c_score=' . $m->r_c_score;
                         ?>
                         <div>
                             <table>
@@ -52,8 +70,8 @@
                                     <td>
                                         <div class="img_wrapper">
                                             <img height="200" width="145" src="<?= $m->img; ?>"/>
-                                            <div class="movie_add_btn">
-                                                <a class="add_link" id="<?= $m->id; ?>" href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=1">Add</a>
+                                            <div class="movie_btn">
+                                                <a class="button_link" id="<?= $m->id; ?>" href="<?= $add_link; ?>">Add</a>
                                             </div>
 
                                         </div>
@@ -63,8 +81,8 @@
                                             <li>Title: <?= $m->title; ?></li>
                                             <li>Year: <?= $m->year; ?></li>
                                             <li>MPAA Rating: <?= $m->m_rating; ?></li>
-                                            <li><a id="<?= $m->id; ?>" href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=1">Add To Viewed List</a></li>
-                                            <li><a href="../rotten/add_movie/<?= $m->id; ?>/?title=<?= urlencode($m->title); ?>&year=<?= $m->year; ?>&img=<?= urlencode($m->img); ?>&m_rating=<?= $m->m_rating; ?>&viewed=0">Add to Wish List!</a></li>
+                                            <li><a id="<?= $m->id; ?>" href="<?= $add_link;?>">Add To Viewed List</a></li>
+                                            <li><a href="<?=$add_link;?>">Add to Wish List!</a></li>
                                         </ul>
                                     </td>
                                 </tr>
