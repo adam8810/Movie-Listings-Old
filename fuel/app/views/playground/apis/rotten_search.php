44bc540d9d67449c13d5d3c237244f5ba15b1145
Adam Booth <adam@ambooth.com>
2012-03-15 01:43:53 -0500

    This file is now out of date... added logic for release_(theater,dvd)
    Also added an easier method to create link for both wish and viewed lists

--- fuel/app/views/playground/apis/rotten_search.php
+++ fuel/app/views/playground/apis/rotten_search.php
@@ -52,17 +52,32 @@
                 {
                     foreach ($movie as $m)
                     {
-                        if(isset($m->r_c_rating))
-                        $temp_r_c_rating = $m->r_c_rating;
+                        if (isset($m->r_c_rating))
+                            $temp_r_c_rating = $m->r_c_rating;
                         else
                             $temp_r_c_rating = 'None';
-                        
-                        if(isset($m->r_a_rating))
+
+                        if (isset($m->r_a_rating))
                             $temp_r_a_rating = $m->r_a_rating;
                         else
                             $temp_r_a_rating = 'None';
+
+                        if (isset($m->release_dvd))
+                            $temp_release_dvd = $m->release_dvd;
+                        else
+                            $temp_release_dvd = 'NA';
+
+
+                        if (isset($m->release_theater))
+                            $temp_release_theater = $m->release_theater;
+                        else
+                            $temp_release_theater = 'NA';
                         
-                        $add_link = '../rotten/add_movie/' . $m->id . '/?title=' . urlencode($m->title) . '&year=' . $m->year . '&img=' . urlencode($m->img) . '&m_rating=' . $m->m_rating . '&viewed=1&r_a_rating=' . $temp_r_a_rating . '&r_c_rating='.$temp_r_c_rating. '&r_a_score=' . $m->r_a_score . '&r_c_score=' . $m->r_c_score;
+                         $add_link_1 = '../rotten/add_movie/' . $m->id . '/?title=' . urlencode($m->title) . '&year=' . $m->year . '&img=' . urlencode($m->img) . '&m_rating=' . $m->m_rating . '&viewed=';
+                         $add_link_2 = '&r_a_rating=' . $temp_r_a_rating . '&r_c_rating=' . $temp_r_c_rating . '&r_a_score=' . $m->r_a_score . '&r_c_score=' . $m->r_c_score . '&runtime=' . $m->runtime . '&release_dvd=' . $temp_release_dvd . '&release_theater=' . $temp_release_theater;
+                         
+                         $add_link_wishlist = $add_link_1 . 0 . $add_link_2;
+                         $add_link_viewed = $add_link_1 . 1 . $add_link_2;
                         ?>
                         <div>
                             <table>
@@ -71,7 +86,7 @@
                                         <div class="img_wrapper">
                                             <img height="200" width="145" src="<?= $m->img; ?>"/>
                                             <div class="movie_btn">
-                                                <a class="button_link" id="<?= $m->id; ?>" href="<?= $add_link; ?>">Add</a>
+                                                <a class="button_link" id="<?= $m->id; ?>" href="<?= $add_link_viewed; ?>">Add</a>
                                             </div>
 
                                         </div>
@@ -79,10 +94,11 @@
                                     <td class="movie_info">
                                         <ul>
                                             <li>Title: <?= $m->title; ?></li>
+                                            <li>Runtime: <?= (floor($m->runtime / 60) > 1 ? floor($m->runtime / 60) . ' hours' : floor($m->runtime / 60) . ' hour'); ?> and <?= $m->runtime % 60; ?> minutes</li>
                                             <li>Year: <?= $m->year; ?></li>
                                             <li>MPAA Rating: <?= $m->m_rating; ?></li>
-                                            <li><a id="<?= $m->id; ?>" href="<?= $add_link;?>">Add To Viewed List</a></li>
-                                            <li><a href="<?=$add_link;?>">Add to Wish List!</a></li>
+                                            <li><a id="<?= $m->id; ?>" href="<?= $add_link_viewed; ?>">Add To Viewed List</a></li>
+                                            <li><a href="<?= $add_link_wishlist; ?>">Add to Wish List!</a></li>
                                         </ul>
                                     </td>
                                 </tr>
