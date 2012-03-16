f928d946c6589a4941af68c7d0f98bfd425f9b61
Adam Booth <adam@ambooth.com>
2012-03-16 11:54:11 -0500

    Modified the primary session control
    Switched from cookies to db

--- fuel/core/config/session.php
+++ fuel/core/config/session.php
@@ -30,7 +30,7 @@ return array(
 	'auto_initialize'	=> true,
 
 	// if no session type is requested, use the default
-	'driver'			=> 'cookie',
+	'driver'			=> 'db',
 
 	// check for an IP address match after loading the cookie (optional, default = false)
 	'match_ip'			=> false,
@@ -91,7 +91,7 @@ return array(
 
 	// specific configuration settings for database based sessions
 	'db'			=> array(
-		'cookie_name'		=> 'fueldid',				// name of the session cookie for database based sessions
+		'cookie_name'		=> 'movielist',				// name of the session cookie for database based sessions
 		'database'			=> null,					// name of the database name (as configured in config/db.php)
 		'table'				=> 'sessions',				// name of the sessions table
 		'gc_probability'	=> 5						// probability % (between 0 and 100) for garbage collection
