22621b3686d10f1551e43364d3193fb50d531423
Adam Booth <adam@ambooth.com>
2012-03-16 19:56:33 -0500

    Updated colors and such

--- public/assets/js/uservoice.js
+++ public/assets/js/uservoice.js
@@ -1,9 +1,6 @@
 var uvOptions = {};
-(function() {
-    var uv = document.createElement('script');
-    uv.type = 'text/javascript';
-    uv.async = true;
+  (function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
     uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/8vzbKztcrJBBSsXCGRgBQ.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(uv, s);
-})();
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+  })();
