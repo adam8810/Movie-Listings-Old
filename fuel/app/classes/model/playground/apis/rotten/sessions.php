f24ff0a5825670357af5d2ac2e46752f7ddabaf0
Adam Booth <adam@ambooth.com>
2012-03-19 13:12:54 -0500

    Added session static functions to get and set session info

--- fuel/app/classes/model/playground/apis/rotten/sessions.php
+++ fuel/app/classes/model/playground/apis/rotten/sessions.php
@@ -1,10 +1,30 @@
 <?php
-namespace Model\Playground\Apis\Rotten\Sessions;
+namespace Model\Playground\Apis\Rotten;
+
+use Fuel\Core\Session;
+
 
 class Sessions extends \Model
 {
-    public static function set_var()
+    public static function set_username()
+    {        
+        Session::set('userid', 'adam8810');
+    }
+    
+    public static function get_username()
     {
-        
+        echo Session::get('userid');
+    }
+    
+    public static function set_search($search_query)
+    {
+        // Sets the search query in the session
+        Session::set('search_query', $search_query);
+    }
+    
+    public static function get_search()
+    {
+        // Gets the search query in the session
+        return Session::get('search_query');
     }
 }
